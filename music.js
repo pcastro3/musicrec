8059fdc0d9a7b964569855582b5587242d1576b8
Patrick Castro <patrickcastro117@yahoo.com>
2020-06-16 17:40:18 -0700

    added artist fetch

--- music.js
+++ music.js
@@ -14,18 +14,16 @@
 
 // &artist=coldplay
 
+// INPUT ARTIST AND FETCH SIMILAR
+
 async function getArtist() {
-  const result = await fetch('http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&&artist=(${artist})=a11aa08044485cfbb982b3336aa0317c&format=json');
-  // const artist = input.value();
-  // const group = [];
-  // band.input.value();
+  const input = await document.getElementById('artist').value;
+  const artist = 'cher';
+
+  const result = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&&artist=${input}&api_key=a11aa08044485cfbb982b3336aa0317c&format=json`);
+
   const data = await result.json();
-  // const input;
   console.log(data);
 }
-getArtist();
 
-async function insert() {
-  input = await document.getElementById('artist').value;
-  console.log(input);
-}
+getArtist();
