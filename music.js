b1b6b4f30d8df29f3f0d10d722ca6a9c53139847
Patrick Castro <patrickcastro117@yahoo.com>
2020-06-19 17:05:22 -0700

    completed fetching and added css animations

--- music.js
+++ music.js
@@ -1,29 +1,21 @@
 // fetch('http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=cher&api_key=a11aa08044485cfbb982b3336aa0317c&format=json')
-// .then(result => {
-//   console.log(result);
-//   return result.json();
-// })
-// .then(data => {
-//   // console.log(data);
-//   const artist = data.similarartists[1];
-//   console.log('This artist is ${data.artist}');
-// })
-// .catch(error => {
-//   console.log(error);
-// });
 
 // &artist=coldplay
 
 // INPUT ARTIST AND FETCH SIMILAR
 
 async function getArtist() {
-  const input = await document.getElementById('artist').value;
-  // const artist = 'cher';
+  const input = await document.querySelector('.artist').value;
 
   const result = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&&artist=${input}&api_key=a11aa08044485cfbb982b3336aa0317c&format=json`);
 
   const data = await result.json();
-  const name = data.similarartists.artist[0].name;
-  document.getElementById('name').textContent = name;
-  console.log(data);
+  const get = document.querySelectorAll('.name');
+  const five = [];
+
+  for (var i = 0; i < 5; i++) {
+    five.push(data.similarartists.artist[i].name);
+    get[i].textContent = five[i]
+  }
+
 }
