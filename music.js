5b86b891ff9bbc42d66ed88ff49c686d5a91f2cf
Patrick Castro <patrickcastro117@yahoo.com>
2020-06-22 16:03:31 -0700

    hide and show table functionality

--- music.js
+++ music.js
@@ -2,7 +2,7 @@
 
 // &artist=coldplay
 
-// INPUT ARTIST AND FETCH SIMILAR
+// INPUT ARTIST AND FETCH 5 SIMILAR
 
 async function getArtist() {
   const input = await document.querySelector('.artist').value;
@@ -10,12 +10,23 @@ async function getArtist() {
   const result = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&&artist=${input}&api_key=a11aa08044485cfbb982b3336aa0317c&format=json`);
 
   const data = await result.json();
-  const get = document.querySelectorAll('.name');
-  const five = [];
+  const get = await document.querySelectorAll('.name');
+  const hide = await document.getElementById('hidden');
+  const five = await [];
 
   for (var i = 0; i < 5; i++) {
     five.push(data.similarartists.artist[i].name);
     get[i].textContent = five[i]
   }
+
+  if (get === true) {
+    hide.style.display = 'none';
+  } else {
+    hide.style.display = 'block';
+  }
+
+  // GET PHOTO THRU API TRACK.GETINFO
+  // DISPLAY TOP 3 SONGS THRU ARTIST.GETTOPTRACKS FOR EACH OF THE 5 RETURNED ARTISTS
+  // ADD URL SO RECOMMENDATION GIVES USER OPTION TO LISTEN TO SONG ON LASTFM
 
 }
