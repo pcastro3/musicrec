53a64d63341920b92dea06e11f3465dac6c203ad
Patrick Castro <patrickcastro117@yahoo.com>
2020-06-25 15:54:50 -0700

    fetched top tracks, fetched urls, added grid styling

--- music.js
+++ music.js
@@ -1,7 +1,5 @@
 // fetch('http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=cher&api_key=a11aa08044485cfbb982b3336aa0317c&format=json')
 
-// &artist=coldplay
-
 // INPUT ARTIST AND FETCH 5 SIMILAR
 
 async function getArtist() {
@@ -20,10 +18,14 @@ async function getArtist() {
   const name = await document.querySelectorAll('.name');
   const image = await document.querySelectorAll('.image');
   const track = await document.querySelectorAll('.track');
+  const url = await document.querySelectorAll('.url');
+  const top = await document.querySelectorAll('.top');
 
   const name5 = await [];
   const image5 = await [];
   const track3 = await [];
+  const url5 = await [];
+  const top5 = await [];
 
   for (var i = 0; i < 5; i++) {
     name5.push(data.similarartists.artist[i].name);
@@ -35,14 +37,20 @@ async function getArtist() {
     image5.push(data2[i].topalbums.album[0].image[2]['#text']);
     image[i].src = image5[i];
 
-    api3[i] = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${track3[i]}&api_key=a11aa08044485cfbb982b3336aa0317c&format=json`);
+    url5.push(data2[i].topalbums.album[0].url)
+    url[i].href = url5[i];
+
+    api3[i] = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${name5[i]}&api_key=a11aa08044485cfbb982b3336aa0317c&format=json`);
 
     data3[i] = await api3[i].json();
-    track3.push(data3[i].toptracks.track[i].name);
+    track3.push(data3[i].toptracks.track[0].name);
     track[i].textContent = track3[i];
 
-    console.log(track[i]);
+    top5.push(data3[i].toptracks.track[0].url);
+    top[i].href = top5[i];
   }
+
+  // HIDE THE TABLE
 
   const hide = await document.getElementById('hidden');
 
@@ -51,9 +59,5 @@ async function getArtist() {
   } else {
     hide.style.display = 'block';
   }
-
-  // GET PHOTO THRU API TRACK.GETINFO [--------------------CHECK]
-  // DISPLAY TOP 3 SONGS THRU ARTIST.GETTOPTRACKS FOR EACH OF THE 5 RETURNED ARTISTS
-  // ADD URL SO RECOMMENDATION GIVES USER OPTION TO LISTEN TO SONG ON LASTFM
 
 }
